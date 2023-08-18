268d6d7f860f415233120e8491c95b6f645a3c18
lzente <142539342+lzente@users.noreply.github.com>
2023-08-18 11:03:43 -0400

    Update leagueTeamManagers.js

--- src/lib/utils/helperFunctions/leagueTeamManagers.js
+++ src/lib/utils/helperFunctions/leagueTeamManagers.js
@@ -42,7 +42,7 @@ export const getLeagueTeamManagers = async () => {
         }
         for(const roster of rosters) {
             teamManagersMap[year][roster.roster_id] = {
-                team: getTeamData(processedUsers, roster.owner_id),
+                team: getTeamData(processedUsers, roster.roster_id),
                 managers: getManagers(roster, processedUsers),
             };
         }
